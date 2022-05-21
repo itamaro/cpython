b99ee7870951491a5835033dac2a918f6720ec2b
Itamar Ostricher <itamarost@gmail.com>
2022-05-20 18:27:31 -0700

    Change compiler to emit new EAGER_IMPORT_NAME opcode
    Logic:
    - `IMPORT_NAME` is emitted for module-level imports with zero fblocks
      these imports _may_ be loaded lazily (if running with lazy imports enabled)
    - `EAGER_IMPORT_NAME` is emitted in all other cases
      these imports will _always_ load eagerly
    
    for example, the following snippet emits only `IMPORT_NAME`
    (since it has only module-level imports, including inside if blocks)
    
    ```
    $ ./python -P -L -c 'import dis; dis.dis("""
    import foo as bar
    from spam import ham
    
    if condition:
      from a import b as c
    else:
      import c
    """)' | grep IMPORT
                  6 IMPORT_NAME              0 (foo)
                 14 IMPORT_NAME              2 (spam)
                 16 IMPORT_FROM              3 (ham)
                 30 IMPORT_NAME              5 (a)
                 32 IMPORT_FROM              6 (b)
                 46 IMPORT_NAME              7 (c)
    ```
    
    and another example, showing some cases where the imports are eager:
    
    ```
    $ ./python -P -L -c 'import dis; dis.dis("""
    try:
      import spam
      from a import b as c
    except:
      pass
    
    with eager_imports():
      from a import b as c
      import ham
    
    def my_foo():
      import foo as bar
      from a import b as c
      return bar.do(c.get())
    
    class Foo:
      import foo as bar
      from a import b as c
    """)' | grep IMPORT
                  8 EAGER_IMPORT_NAME        0 (spam)
                 16 EAGER_IMPORT_NAME        1 (a)
                 18 IMPORT_FROM              2 (b)
                 62 EAGER_IMPORT_NAME        1 (a)
                 64 IMPORT_FROM              2 (b)
                 74 EAGER_IMPORT_NAME        5 (ham)
                  6 EAGER_IMPORT_NAME        0 (foo)
                 14 EAGER_IMPORT_NAME        1 (a)
                 16 IMPORT_FROM              2 (b)
                 14 EAGER_IMPORT_NAME        3 (foo)
                 22 EAGER_IMPORT_NAME        5 (a)
                 24 IMPORT_FROM              6 (b)
    ```

--- Python/compile.c
+++ Python/compile.c
@@ -1624,6 +1624,15 @@ compiler_addop_j_noline(struct compiler *c, int opcode, basicblock *b)
         return 0; \
 }
 
+#define ADDOP_IMPORT(C, O, TYPE) { \
+    if (((C)->u->u_scope_type == COMPILER_SCOPE_MODULE) \
+        && ((C)->u->u_nfblocks == 0)) { \
+        ADDOP_NAME((C), IMPORT_NAME, (O), TYPE); \
+    } else { \
+        ADDOP_NAME((C), EAGER_IMPORT_NAME, (O), TYPE); \
+    } \
+}
+
 #define ADDOP_I(C, OP, O) { \
     if (!compiler_addop_i((C), (OP), (O))) \
         return 0; \
@@ -3886,7 +3895,7 @@ compiler_import(struct compiler *c, stmt_ty s)
 
         ADDOP_LOAD_CONST(c, zero);
         ADDOP_LOAD_CONST(c, Py_None);
-        ADDOP_NAME(c, IMPORT_NAME, alias->name, names);
+        ADDOP_IMPORT(c, alias->name, names);
 
         if (alias->asname) {
             r = compiler_import_as(c, alias->name, alias->asname);
@@ -3941,11 +3950,11 @@ compiler_from_import(struct compiler *c, stmt_ty s)
     ADDOP_LOAD_CONST_NEW(c, names);
 
     if (s->v.ImportFrom.module) {
-        ADDOP_NAME(c, IMPORT_NAME, s->v.ImportFrom.module, names);
+        ADDOP_IMPORT(c, s->v.ImportFrom.module, names);
     }
     else {
         _Py_DECLARE_STR(empty, "");
-        ADDOP_NAME(c, IMPORT_NAME, &_Py_STR(empty), names);
+        ADDOP_IMPORT(c, &_Py_STR(empty), names);
     }
     for (i = 0; i < n; i++) {
         alias_ty alias = (alias_ty)asdl_seq_GET(s->v.ImportFrom.names, i);
